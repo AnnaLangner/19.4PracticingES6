3aa90baade3034b795900d9f53c13af5d40e51ab
Ania <annalangner06@gmail.com>
2019-05-28 21:51:02 +0200

    adding the second task

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-// zadanie 1
+// task 1
 
 const a = 'Hello';
 const b = 'World';
 const c = `${a} ${b}`;
 console.log(c);
+
+// task 2
+
+const multiply = (x, y = 1) => console.log(x * y);
+multiply(5);
 
