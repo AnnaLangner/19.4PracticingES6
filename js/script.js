934ada0c8a9dac8328b01b5a14d2a24dae213c8e
Ania <annalangner06@gmail.com>
2019-05-29 11:56:41 +0200

    adding the third task

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ console.log(c);
 const multiply = (x, y = 1) => console.log(x * y);
 multiply(5);
 
+//task 3
+
+const average = (...args) => {
+    let sum = 0;
+    //args.forEach(arg => sum += arg);
+    for(let i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+    console.log(sum / args.length);
+};
+average(1,2,3,4);
+
+//
